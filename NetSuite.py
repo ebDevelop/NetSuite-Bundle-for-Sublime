b6ee4c0bac931da9e3db81832a580d886fbc5281
Thalles <ththev@gmail.com>
2015-07-11 12:00:22 -0400

    Added sublist IDs to the NS Menu
    - Changed getRecordTypeId function signature to insertRecordTypeId;
    - Added sublist IDs to the NS Menu;

--- NetSuite.py
+++ NetSuite.py
@@ -1,9 +1,9 @@
 import sublime, sublime_plugin, json
 
-mainMenu = ['Record Types', 'Templates']
+mainMenu = ['Record Type IDs', 'Templates', 'Sublist IDs']
 
 def plugin_loaded():
-    # Load Record Types
+    # Load Record Type IDs
     global recordTypeOptions, recordTypes
     recordTypeOptions = []
     recordTypesJsonString = sublime.load_resource("/".join(["Packages", __package__, "RecordTypes.json"]))
@@ -19,27 +19,40 @@ def plugin_loaded():
         menuItem = [record['name'], record['description']]
         templateOptions.append(menuItem)
 
+    # Load Sublist IDs
+    global sublistOptions, sublists
+    sublistOptions = []
+    sublistOptionsJsonString = sublime.load_resource("/".join(["Packages", __package__, "SublistIDs.json"]))
+    sublists = json.loads(sublistOptionsJsonString)
+    for sublist in sublists:
+        menuItem = [sublist['name'], sublist['location']]
+        sublistOptions.append(menuItem)
+
 class NetsuiteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         currentSelection = self.view.substr(self.view.sel()[0])
         if currentSelection== 'type':
             self.showSubmenu(0)
+        if currentSelection== 'sublist':
+            self.showSubmenu(2)
         else:
             self.view.window().show_quick_panel(mainMenu, self.showSubmenu)
 
     def showSubmenu(self, id):
-        # Record Types
+        # Record Type IDs
         if id==0:
-            self.view.window().show_quick_panel(recordTypeOptions, self.getRecordTypeId)
+            self.view.window().show_quick_panel(recordTypeOptions, self.insertRecordTypeId)
         # Templates
         if id==1:
             self.view.window().show_quick_panel(templateOptions, self.insertSnippet)
+        if id==2:
+            self.view.window().show_quick_panel(sublistOptions, self.insertSublistId)
 
-    def getRecordTypeId(self, id):
+    def insertRecordTypeId(self, id):
         # Back
         if id==0:
             self.view.window().show_quick_panel(mainMenu, self.showSubmenu)
-        # A Record was selected
+        # A Record Type was selected
         if id>0:
             recordid = recordTypes[id]['internalid'];
             self.view.run_command("insert", {"characters": recordid})
@@ -52,4 +65,12 @@ class NetsuiteCommand(sublime_plugin.TextCommand):
         if id>0:
             #insert snippet
             self.view.run_command("insert_snippet", {"name": "/".join(["Packages", __package__, "Template Files", templates[id]['file']]) })
-            #testing branch!
+
+    def insertSublistId(self, id):
+        # Back
+        if id==0:
+            self.view.window().show_quick_panel(mainMenu, self.showSubmenu)
+        # A Record Type was selected
+        if id>0:
+            sublistId = sublists[id]['internalid'];
+            self.view.run_command("insert", {"characters": sublistId})
