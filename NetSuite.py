f77a20f1870d9421f6299c2cb1c69a6f39e6e60b
Thalles <ththev@gmail.com>
2015-07-11 23:48:21 -0400

    I'm not sure why this file was not included in the previous commit
    535b4c5bedd0835d5831c25a8b7f8c333b55dd51

--- NetSuite.py
+++ NetSuite.py
@@ -1,19 +1,17 @@
 import sublime, sublime_plugin, json
 
-mainMenu = ['Record Type IDs', 'Templates', 'Sublist IDs']
-
 def plugin_loaded():
-    # Convert Files to Arrays
-    global recordTypeIds, sublistIds, templates
-    recordTypeIds = parseFile('RecordTypes.json')
-    sublistIds = parseFile('SublistIDs.json')
-    templates = parseFile('Templates.json')
+    # Convert the main menu in an Array
+    global mainMenu
+    mainMenu = parseFile('MainMenu.json')
 
+# This function reads a JSON file in package folder and returns an Array
 def parseFile(file):
     fileContents = sublime.load_resource("/".join(["Packages", __package__, file]))
     jsonObj = json.loads(fileContents)
     return jsonObj
 
+# Returns the 'name' and 'memo' fileds from a menu array to be used as menu options
 def getMenuItems(optionsArray, includeMemo):
     menuItems = []
     if includeMemo:
@@ -25,41 +23,57 @@ def getMenuItems(optionsArray, includeMemo):
     return menuItems
 
 class NetsuiteCommand(sublime_plugin.TextCommand):
-    # Show Main Menu. Depending on the text selected, go directly to the submenu
+    # Show Main Menu or depending on the text selected, go directly to the submenu
     def run(self, edit):
+        # if the text selected is the same as the submeny keyword, go directly to the submenu
         currentSelection = self.view.substr(self.view.sel()[0])
-        if currentSelection== 'type':
-            self.showSubmenu(0)
-        if currentSelection== 'sublist':
-            self.showSubmenu(2)
-        else:
-            self.view.window().show_quick_panel(mainMenu, self.showSubmenu)
+        if currentSelection:
+            for submenu in mainMenu:
+                if submenu['keyword'] == currentSelection:
+                    self.showSubmenu(mainMenu.index(submenu))
+                    break
+        self.showMainMenu();
 
     def showSubmenu(self, menuId):
-        # Record Type IDs
-        if menuId==0:
-            self.view.window().show_quick_panel(getMenuItems(recordTypeIds, True), lambda id: self.insertInternalId(id, recordTypeIds))
-        # Templates
-        if menuId==1:
-            self.view.window().show_quick_panel(getMenuItems(templates, True), self.insertSnippet)
-        #sublist IDs
-        if menuId==2:
-            self.view.window().show_quick_panel(getMenuItems(sublistIds, True), lambda id: self.insertInternalId(id, sublistIds))
+        if menuId==-1:
+            return
+        # Get the default action from the Main Menu file
+        action = mainMenu[menuId]['defaultAction']
+        # Get the file where the submenu is defined
+        fileName = mainMenu[menuId]['internalid']
+        # Check if the memo field needs to be included in the submenu options
+        showMemo = mainMenu[menuId]['showMemo']
+        # Convert the submenu file in an Array
+        submenuArray = parseFile(fileName)
+        # Show the submenu
+        self.view.window().show_quick_panel(getMenuItems(submenuArray, showMemo), lambda id: self.executeAction(id, action, submenuArray))
+
+    # execute the menu action
+    def executeAction(self, id, action, optionsArray):
+        if id==-1:
+            return
+        #override the default action, if any
+        try :
+            if optionsArray[id]['action']:
+                action = optionsArray[id]['action']
+        except:
+            pass
+        # Execute the menu option action
+        if action=='insertInternalId':
+            self.insertInternalId(id, optionsArray)
+        if action=='insertSnippet':
+            self.insertSnippet(id, optionsArray)
+        if action=='showMainMenu':
+            self.showMainMenu()
+
+    # Menu Actions:
 
     def insertInternalId(self, id, optionsArray):
-        # Back
-        if id==0:
-            self.view.window().show_quick_panel(mainMenu, self.showSubmenu)
-        # A menu item was selected
-        if id>0:
-            internalid = optionsArray[id]['internalid'];
-            self.view.run_command("insert", {"characters": internalid})
+        internalid = optionsArray[id]['internalid'];
+        self.view.run_command("insert", {"characters": internalid})
 
-    def insertSnippet(self, id):
-        # Back
-        if id==0:
-            self.view.window().show_quick_panel(mainMenu, self.showSubmenu)
-        # A Template was selected
-        if id>0:
-            #insert snippet
-            self.view.run_command("insert_snippet", {"name": "/".join(["Packages", __package__, "Template Files", templates[id]['internalid']]) })
+    def insertSnippet(self, id, optionsArray):
+        self.view.run_command("insert_snippet", {"name": "/".join(["Packages", __package__, "Template Files", optionsArray[id]['internalid']]) })
+
+    def showMainMenu(self):
+        self.view.window().show_quick_panel(getMenuItems(mainMenu, False), self.showSubmenu)
